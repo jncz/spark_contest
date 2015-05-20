d66295b71342d165d5f879a1b6c8de4f4712090b
Ella <ellahaoxinqing@gmaili.com>
2015-05-20 16:18:08 +0800

    inital version for ui part.

---
+++ ui/src/main/java/ui/common/comet/MessageSender.java
@@ -0,0 +1,78 @@
+package ui.common.comet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletResponse;
+
+public class MessageSender implements Runnable {
+
+	protected boolean running = true;
+//	protected final AdrrayList<String> messages = new ArrayList<String>();
+	protected final ArrayList<String> messages = new ArrayList<String>();
+	private ServletResponse connection;
+
+	public synchronized void setConnection(ServletResponse connection) {
+		this.connection = connection;
+		notify();
+	}
+
+	public void stop() {
+		running = false;
+	}
+
+	/**
+	 * Add message for sending.
+	 */
+	public void send(String message) {
+		synchronized (messages) {
+			messages.add(message);
+			System.out.println("Message added #messages=" + messages.size());
+			messages.notify();
+		}
+	}
+
+	public void run() {
+		while (running) {
+			if (messages.size() == 0) {
+				try {
+					synchronized (messages) {
+						messages.wait();
+					}
+				} catch (InterruptedException e) {
+					// Ignore
+				}
+			}
+			String[] pendingMessages = null;
+			synchronized (messages) {
+				pendingMessages = messages.toArray(new String[0]);
+				messages.clear();
+			}
+			try {
+				if (connection == null) {
+					try {
+						synchronized (this) {
+							wait();
+						}
+					} catch (InterruptedException e) {
+						// Ignore
+					}
+				}
+				PrintWriter writer = connection.getWriter();
+				for (int j = 0; j < pendingMessages.length; j++) {
+					final String forecast = pendingMessages[j];
+					writer.println(forecast);
+					System.out.println("Writing:" + forecast);
+				}
+				writer.flush();
+				writer.close();
+				messages.removeAll(messages);
+				connection = null;
+				System.out.println("Closing connection");
+			} catch (IOException e) {
+				System.out.println("IOExeption sending message");
+			}
+		}
+	}
+}
---
+++ ui/src/main/java/ui/common/json/reader/JsonReader.java
@@ -0,0 +1,42 @@
+package ui.common.json.reader;
+
+import java.io.File;
+
+import org.json.simple.JSONObject;
+
+import ui.common.comet.MessageSender;
+import ui.common.json.parser.JsonParser;
+
+public class JsonReader {
+	private MessageSender messageSender;
+	private final String jsonPath = "C:/Ella/work/spark/data/output/latest.json";
+
+	public JsonReader(MessageSender messageSender) {
+		this.messageSender = messageSender;
+	}
+
+	public void start() {
+		Runnable r = new Runnable() {
+
+			public void run() {
+				int counter = 1;
+				while (true) {
+					File jsonFile = new File(jsonPath);
+					if(jsonFile.exists()){
+						JSONObject object = JsonParser.parseJsonFile(jsonPath);
+						messageSender.send(object.toJSONString());
+						System.out.println(counter);
+						counter ++;
+					}
+					try {
+						Thread.sleep(5000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+		Thread t = new Thread(r);
+		t.start();
+	}
+}
